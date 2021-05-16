7c1f92368b86bff4eacf810a44cc59680aa7a42b
Marina Ionel <marinicaionel@gmail.com>
2021-05-16 12:06:39 +0200

    azure

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,8 +1,5 @@
-using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace MoviesApi
 {
@@ -15,13 +12,6 @@ namespace MoviesApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    Uri keyVaultEndpoint = new(Environment.GetEnvironmentVariable("VaultUri"));
-                    config.AddAzureKeyVault(
-                    keyVaultEndpoint,
-                    new DefaultAzureCredential());
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
