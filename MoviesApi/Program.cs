35bc89e99facb06e419f4c8b3a032783e10658da
Marina Ionel <marinicaionel@gmail.com>
2021-05-20 20:40:57 +0200

    Update Program.cs

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using MoviesApi.Common;
 
 namespace MoviesApi
 {
@@ -7,6 +8,7 @@ namespace MoviesApi
     {
         public static void Main(string[] args)
         {
+            Log.Default.Debug("Application started");
             CreateHostBuilder(args).Build().Run();
         }
 
