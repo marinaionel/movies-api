bb26e0f81ff01d31033a6bbfa3e45cd5d4a9f73d
Marina Ionel <marinicaionel@gmail.com>
2021-05-26 18:40:16 +0200

    Update Person.cs

--- MoviesApi.Core/Models/Person.cs
+++ MoviesApi.Core/Models/Person.cs
@@ -9,7 +9,7 @@ namespace MoviesApi.Core.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public long? Birth { get; set; }
+        public int? Birth { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
         public ICollection<Movie> DirectedMovies { get; set; } = new HashSet<Movie>();
